2cf13ebdb19c6124d3ada915a844bd717c889216
Szymon Tamborski <szytamborski@gmail.com>
2022-12-02 12:54:48 +0100

    Initial commit

---
+++ src/main/java/pl/tamborski/npeapi/NpeapiApplication.java
@@ -0,0 +1,13 @@
+package pl.tamborski.npeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpeapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tamborski/npeapi/NpeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tamborski.npeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpeapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
